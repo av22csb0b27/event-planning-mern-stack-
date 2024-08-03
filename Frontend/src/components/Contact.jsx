8526d4b24235354dc6b6cdf4cbf0d5918337e3d2
Anubhav Vaishnav <vaishnav.anubhav20@gmail.com>
2024-08-03 23:24:08 +0530

    Update Contact.jsx

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = ()=>{
 
     const handleSendMessage = async(e)=>{
         e.preventDefault();
-        await axios.post("/send",{name,email,subject,message},{withCredentials:true,headers:{"Content-Type":"application/json"}}).then(res=>{
+        await axios.post("/api/v1/message/send",{name,email,subject,message},{withCredentials:true,headers:{"Content-Type":"application/json"}}).then(res=>{
             toast.success(res.data.message);
             setName("");
             setEmail("");
